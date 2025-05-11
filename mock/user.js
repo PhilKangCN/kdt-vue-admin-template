ccece752d1a8d98f2a83dfe8369d8ea3226bc34f
PhilKangCN <1076018549@qq.com>
2025-05-11 12:34:02 +0800

    生产环境处理

--- mock/user.js
+++ mock/user.js
@@ -48,27 +48,27 @@ module.exports = [
   // },
 
   // get user info
-  {
-    url: '/kdt-vue-admin-template/user/info\.*',
-    type: 'get',
-    response: config => {
-      const { token } = config.query
-      const info = users[token]
-
-      // mock error
-      if (!info) {
-        return {
-          code: 50008,
-          message: 'Login failed, unable to get user details.'
-        }
-      }
-
-      return {
-        code: 20000,
-        data: info
-      }
-    }
-  },
+  // {
+  //   url: '/kdt-vue-admin-template/user/info\.*',
+  //   type: 'get',
+  //   response: config => {
+  //     const { token } = config.query
+  //     const info = users[token]
+  //
+  //     // mock error
+  //     if (!info) {
+  //       return {
+  //         code: 50008,
+  //         message: 'Login failed, unable to get user details.'
+  //       }
+  //     }
+  //
+  //     return {
+  //       code: 20000,
+  //       data: info
+  //     }
+  //   }
+  // },
 
   // user logout
   {
